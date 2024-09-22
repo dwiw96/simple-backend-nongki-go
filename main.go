e7628b6ef530f2074914ea41e36c8c8de45670c7
dwiw96 <dwiwahyudi1996@gmail.com>
2024-09-22 18:33:35 +0700

    add: call all function

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"context"
 	"log"
+	server "simple-backend-nongki-go/api"
 	config "simple-backend-nongki-go/config"
+	factory "simple-backend-nongki-go/factory"
 	postgres "simple-backend-nongki-go/utils/driver/postgresql"
+	password "simple-backend-nongki-go/utils/password"
+	"time"
 )
 
 func main() {
 	log.Println("-- run market maven v2 --")
 	env := config.GetEnvConfig()
 	pgPool := postgres.ConnectToPg(env)
-
 	defer pgPool.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
+	defer cancel()
+
+	password.JwtInit(pgPool, ctx)
+
+	router := server.SetupRouter()
+
+	factory.InitFactory(router, pgPool, ctx)
+
+	server.StartServer(env.SERVER_PORT, router)
 }
